83c7b847e5171fe7f7cd014242616318a50d0169
danielmosazade <danielmosazade@gmail.com>
2025-08-14 16:08:15 +0300

    adding domain page + delete & update user

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require("../middleware/AuthMiddleware");
-const { register, login, deleteUser,updateUsername,getCurrentUser } = require("../controllers/AuthControllers");
+const { register, login, deleteUser,updateUsername,getCurrentUser,getAllUsers,deleteUserById,updateUsernameById } = require("../controllers/AuthControllers");
 
 router.post('/register', register);
 router.post('/login', login);
-router.delete('/users/:username', deleteUser);
+router.delete('/users/by-username/:username', deleteUser);
 router.put('/username', updateUsername);
 router.get('/me', verifyToken, getCurrentUser);
+router.get("/users", verifyToken, getAllUsers);
+router.delete("/users/:id", verifyToken, deleteUserById);
+router.put("/users/:id", verifyToken, updateUsernameById);
+
 module.exports = router;
